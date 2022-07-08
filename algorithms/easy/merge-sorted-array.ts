0c9177fc1095cf0f9728aea30bfaa535e28bbccb
Igor Kalashnikov <igor.kalashnikov@me.com>
2022-07-08 18:55:46 +0300

    + merge sorted array

---
+++ algorithms/easy/merge-sorted-array.ts
@@ -0,0 +1,36 @@
+import tap from "tap";
+
+/**
+ * https://leetcode.com/problems/merge-sorted-array/
+ *
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+ * and two integers m and n, representing the number of elements in nums1
+ * and nums2 respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function, but instead be
+ * stored inside the array nums1. To accommodate this, nums1 has a length
+ * of m + n, where the first m elements denote the elements that should be
+ * merged, and the last n elements are set to 0 and should be ignored.
+ * nums2 has a length of n.
+ */
+
+const merge = (
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void => {
+  let end = m + n - 1;
+  --m;
+  --n;
+
+  while (end >= 0) {
+    nums1[end--] = n < 0 || nums1[m] >= nums2[n] ? nums1[m--] : nums2[n--];
+  }
+};
+
+const num1 = [1, 2, 3, 0, 0, 0];
+merge(num1, 3, [2, 5, 6], 3);
+tap.same(num1, [1, 2, 2, 3, 5, 6]);
