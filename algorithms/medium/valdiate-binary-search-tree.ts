a0f34f2441756f0eb02e091c92c935d9c5863c21
Igor Kalashnikov <igor.kalashnikov@me.com>
2022-07-16 23:14:34 +0300

    + validate binary search tree

---
+++ algorithms/medium/valdiate-binary-search-tree.ts
@@ -0,0 +1,71 @@
+import tap from "tap";
+
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ *
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * A valid BST is defined as follows:
+ *
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ */
+
+// region predefined
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+// endregion
+
+const isValidBST = (
+  root: TreeNode | null,
+  min = -Infinity,
+  max = Infinity
+): boolean => {
+  if (!root) {
+    return true;
+  }
+
+  if (root.val < min || root.val > max) {
+    return false;
+  }
+
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
+};
+
+// region test
+tap.equal(isValidBST(new TreeNode(2, new TreeNode(1), new TreeNode(3))), true);
+
+tap.equal(
+  isValidBST(
+    new TreeNode(
+      5,
+      new TreeNode(1),
+      new TreeNode(4, new TreeNode(3), new TreeNode(6))
+    )
+  ),
+  false
+);
+
+tap.equal(
+  isValidBST(
+    new TreeNode(
+      5,
+      new TreeNode(4),
+      new TreeNode(6, new TreeNode(3), new TreeNode(7))
+    )
+  ),
+  false
+);
+// endregion
