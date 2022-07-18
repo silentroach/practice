5ddce319e675f9319dea9303ae1cb9f208a49056
Igor Kalashnikov <igor.kalashnikov@me.com>
2022-07-18 22:41:29 +0300

    + number of islands

---
+++ algorithms/medium/number-of-islands.ts
@@ -0,0 +1,87 @@
+import tap from "tap";
+
+/**
+ * https://leetcode.com/problems/number-of-islands/
+ *
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land)
+ * and '0's (water), return the number of islands.
+ *
+ * An island is surrounded by water and is formed by connecting adjacent
+ * lands horizontally or vertically. You may assume all four edges of the
+ * grid are all surrounded by water.
+ */
+
+const enum ItemType {
+  Land = "1",
+  Water = "0",
+}
+
+type Grid = string[][];
+
+const removeIsland = (
+  grid: Grid,
+  rowIndex: number,
+  columnIndex: number
+): void => {
+  grid[rowIndex][columnIndex] = ItemType.Water;
+
+  if (rowIndex > 0 && grid[rowIndex - 1][columnIndex] === ItemType.Land) {
+    removeIsland(grid, rowIndex - 1, columnIndex);
+  }
+
+  if (columnIndex > 0 && grid[rowIndex][columnIndex - 1] === ItemType.Land) {
+    removeIsland(grid, rowIndex, columnIndex - 1);
+  }
+
+  if (
+    rowIndex < grid.length - 1 &&
+    grid[rowIndex + 1][columnIndex] === ItemType.Land
+  ) {
+    removeIsland(grid, rowIndex + 1, columnIndex);
+  }
+
+  if (
+    columnIndex < grid[0].length - 1 &&
+    grid[rowIndex][columnIndex + 1] === ItemType.Land
+  ) {
+    removeIsland(grid, rowIndex, columnIndex + 1);
+  }
+};
+
+const numIslands = (grid: Grid): number => {
+  let count = 0;
+
+  // searching for island, count it and remove
+  grid.forEach((row, rowIndex) => {
+    row.forEach((item, columnIndex) => {
+      if (item === ItemType.Land) {
+        ++count;
+        removeIsland(grid, rowIndex, columnIndex);
+      }
+    });
+  });
+
+  return count;
+};
+
+// region tests
+tap.equal(
+  numIslands([
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+  ]),
+  1
+);
+
+tap.equal(
+  numIslands([
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"],
+  ]),
+  3
+);
+// endregion
