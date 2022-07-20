4bc5ee3440ca29fc2ce4b26020c208085c3f1301
Igor Kalashnikov <igor.kalashnikov@me.com>
2022-07-20 23:22:48 +0300

    + reshape the matrix

---
+++ algorithms/easy/reshape-the-matrix.ts
@@ -0,0 +1,71 @@
+import tap from "tap";
+
+/**
+ * https://leetcode.com/problems/reshape-the-matrix/
+ *
+ * In MATLAB, there is a handy function called reshape which can reshape an
+ * m x n matrix into a new one with a different size r x c keeping its original data.
+ *
+ * You are given an m x n matrix mat and two integers r and c representing the
+ * number of rows and the number of columns of the wanted reshaped matrix.
+ *
+ * The reshaped matrix should be filled with all the elements of the original
+ * matrix in the same row-traversing order as they were.
+ *
+ * If the reshape operation with given parameters is possible and legal, output
+ * the new reshaped matrix; Otherwise, output the original matrix.
+ */
+
+const matrixReshape = (mat: number[][], r: number, c: number): number[][] => {
+  const flat = mat.flat();
+  if (r * c !== flat.length) {
+    return mat;
+  }
+
+  const result: number[][] = [];
+  for (let idx = 0; idx < r; idx++) {
+    result.push(flat.slice(idx * c, idx * c + c));
+  }
+
+  return result;
+};
+
+// region tests
+tap.same(matrixReshape([[1]], 2, 2), [[1]]);
+tap.same(
+  matrixReshape(
+    [
+      [1, 2],
+      [3, 4],
+    ],
+    2,
+    2
+  ),
+  [
+    [1, 2],
+    [3, 4],
+  ]
+);
+tap.same(
+  matrixReshape(
+    [
+      [1, 2],
+      [3, 4],
+    ],
+    4,
+    1
+  ),
+  [[1], [2], [3], [4]]
+);
+tap.same(
+  matrixReshape(
+    [
+      [1, 2],
+      [3, 4],
+    ],
+    1,
+    4
+  ),
+  [[1, 2, 3, 4]]
+);
+// endregion
