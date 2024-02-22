1f04b61ba8c25cccc3116ac867b3383ccc1099d4
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-02-22 21:38:58 +0400

    + counter

---
+++ leetcode/easy/counter.ts
@@ -0,0 +1,13 @@
+/**
+ * Counter
+ * https://leetcode.com/problems/counter
+ *
+ * Given an integer n, return a counter function. This counter function
+ * initially returns n and then returns 1 more than the previous value every
+ * subsequent time it is called (n, n + 1, n + 2, etc).
+ */
+
+const createCounter =
+  (n: number): (() => number) =>
+  () =>
+    n++;
